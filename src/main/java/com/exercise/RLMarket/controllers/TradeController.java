29ef3774b665c24558f326e3127370837e14896a
v1talick <vitalicivanov04@gmail.com>
2023-12-06 11:05:54 +0200

    form checkpoint

--- src/main/java/com/exercise/RLMarket/controllers/TradeController.java
+++ src/main/java/com/exercise/RLMarket/controllers/TradeController.java
@@ -1,8 +1,16 @@
 package com.exercise.RLMarket.controllers;
 
+import com.exercise.RLMarket.DTOs.ItemDTO;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class TradeController {
@@ -10,4 +18,16 @@ public class TradeController {
     public String home(Model model) {
         return "home";
     }
+//    @GetMapping("/trade")
+//    public String createTradeOffer(Model model) {
+//
+//        return "createTradeOffer";
+//    }
+    @PostMapping("/submitItemList")
+    public String submitTrade(@ModelAttribute("items") ArrayList<ItemDTO> items) {
+        System.out.println(items);
+
+        return "redirect:/";
+    }
+
 }
--- src/main/java/com/exercise/RLMarket/mappers/ItemMapper.java
+++ src/main/java/com/exercise/RLMarket/mappers/ItemMapper.java
@@ -3,19 +3,47 @@ package com.exercise.RLMarket.mappers;
 import com.exercise.RLMarket.DTOs.ItemDTO;
 import com.exercise.RLMarket.enteties.Item;
 
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
 public class ItemMapper {
+    private static final Map<Integer, String> slotsNames = initSlotsNames();
+
+    private static Map<Integer, String> initSlotsNames() {
+        Map<Integer, String> slots = new HashMap<>();
+        slots.put(16, "Banners");
+        slots.put(4, "Antenna");
+        slots.put(0, "Bodies");
+        slots.put(3, "Boost");
+        slots.put(20, "Borders");
+        slots.put(5, "Topper");
+        slots.put(7, "Paint");
+        slots.put(15, "Goal Explosion");
+        slots.put(14, "Supersonic Trails");
+        slots.put(2, "Wheel");
+
+        return Collections.unmodifiableMap(slots);
+    }
+
     public static ItemDTO itemToItemDTO(Item item) {
         Map<Integer,String> slotInterpretation;
-
-        return ItemDTO.builder()
+        ItemDTO itemDTO = ItemDTO.builder()
                 .id(item.getItemId())
                 .itemBlueprint(item.isItemBlueprint())
                 .itemPaintable(item.isItemPaintable())
                 .itemLongLabel(item.getItemLongLabel())
-                .itemType("" + item.getSlotIndex())
                 .build();
+
+        if(slotsNames.containsKey(item.getSlotIndex())) {
+            itemDTO.setItemType(slotsNames.get(item.getSlotIndex()));
+        }
+
+        return itemDTO;
+    }
+
+    public static String itemImageUrl(ItemDTO item) {
+        return "https://github.com/enzanki-ars/rocket-league-item-images/tree/enzanki-ars/Rocket%20League%20Item%20Images/"+item.getItemType()+"/"+item.getItemLongLabel().replace(" ", "%20")+"/Default.png";
     }
 
 }
